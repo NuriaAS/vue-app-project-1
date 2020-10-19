04e31251ea3f12bd47f7d8ed81df6122632972f0
nuriaandresfoto <nuriaandresfoto@gmail.com>
2020-10-19 10:45:12 +0200

    First Commit

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Log in</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login',
+ 
+}
+</script>
---
+++ src/views/Registro.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Registro</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Registro',
+ 
+}
+</script>
